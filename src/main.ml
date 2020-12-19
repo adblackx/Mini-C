8d19872893ed974a4e40ed165bb806b9c60c504b
adblackx <adblackx@gmail.com>
2020-12-19 21:36:21 +0100

    fix error

--- src/main.ml
+++ src/main.ml
@@ -1,13 +1,16 @@
+
+
 let () =
-try 
+ 
   let fichier = Sys.argv.(1) in
   let c = open_in fichier in
+  try
   let lexbuf = Lexing.from_channel c in
   let prog = Mini_c_parser.prog Mini_c_lexer.token lexbuf in
   let test = Mini_c_types.eval_prog prog Mini_c_types.Env.empty in
   ignore(prog);
   close_in c;
  with
-      | Eof ->
-        close_in fichier;
+      | Mini_c_lexer.Eof ->
+        close_in c;
   exit 0
