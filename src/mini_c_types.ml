c15b56d053ff827b08d3e0b5458ddc2521322fd0
Romain <romainmussard974@gmail.com>
2020-12-19 16:38:17 +0100

    Fix Bug Synatxique Verificateur

--- src/mini_c_types.ml
+++ src/mini_c_types.ml
@@ -3,48 +3,140 @@
 *)
 (*open Mini_c*)
 
-type typ = (*types des fonctions*)
-    | Int
-    | Bool
-    | Void
-;;
+  
 
+  
+
+type typ = (*types des fonctions*)
+  | Int
+  | Bool
+  | Void
+
+type expr =
+    | Cst  of int
+    | Add  of expr * expr
+    | Mul  of expr * expr
+    | Lt   of expr * expr
+    | Get  of string
+    | Call of string * expr list
+
+type decla = (*types pour le pasrser*)
+| Boolean of bool
+| Expr of expr
+| Empty
+
+type instr =
+| Putchar of expr
+| Set     of string * expr
+| If      of expr * seq * seq
+| While   of expr * seq
+| Return  of expr
+| Expr    of expr
+and seq = instr list
+
+    
 type typage =
   | Typ of typ
   | TypFun of typ list * typ
-;;
 
-      
+
+type fun_def = { 
+  name:   string;
+  params: (string * typ) list;
+  return: typ;
+  locals: (string * typ * decla) list;
+  code:   seq;
+}  
+
+type prog = {
+  globals:   (string * typ * decla) list; 
+  functions: fun_def list;
+}
+
 module Env = Map.Make(String)
 
-(*vérifie que le prog est bien typé
-  p est de type prog
-  env est de type (string * typ) list *)
-let rec eval_prog p env =
-  let env = eval_declaration p.globals env in
-  eval_functions p.functions env
+
+
+
+
+
+let rec extract_typ p l =
+  match p with
+  | (str,tp)::tl -> extract_typ tl (tp::l)
+  | _ -> l
 ;;
 
+
+(*Renvoie un env, pas besoin d'evaluation ici
+  l est de type (string * typ) list
+  env est de type (string * typ) list *)
+let rec eval_params l env =
+  match l with
+  | (s,t)::tl -> let env = Env.add s (Typ t) env in
+                 eval_params tl env
+  | [] -> env
+;;
+
+
+
+
+(**Evalue les expressions**)
+let rec expr_eval (e: expr) (env: typage Env.t): typage = match e with
+
+  | Cst _ -> Typ(Int)
+
+  | Add(e1, e2) ->
+    let t1 = expr_eval e1 env in
+    let t2 = expr_eval e2 env in
+    if t1 = Typ(Int) && t2 = Typ(Int)
+    then Typ(Int)
+    else failwith "type error"
+
+  | Mul(e1, e2) ->
+    let t1 = expr_eval e1 env in
+    let t2 = expr_eval e2 env in
+    if t1 = Typ(Int) && t2 = Typ(Int)
+    then Typ(Int)
+    else failwith "type error"
+
+  | Lt(e1, e2) ->
+    let t1 = expr_eval e1 env in
+    let t2 = expr_eval e2 env in
+    if t1 = Typ(Int) && t2 = Typ(Int)
+    then Typ(Bool)
+    else failwith "type error"
+
+  | Get(s) -> Env.find s env
+
+  | Call(f, arg) ->
+    let tf = Env.find f env in
+    begin match tf with
+    | TypFun(l, tr) -> if compare_type l arg env
+                       then Typ(tr)
+                       else failwith "type error"
+    | _ -> failwith "type error"
+    end
+and 
+  compare_type l0 l1 env =
+  match l0, l1 with
+  | (ta::tl0, tb::tl1) -> let tb = eval_expr b env in
+                           if ta = tb then compare_type tl0 tl1 env
+                         else false
+  | ([], []) -> true
+  | _ -> false
+;;
 
 (**Renvoie un env et véirifie que la déclaration est bien typé**)
 (**Utilisé pour globals et locals **)
 (**l est de type (string * typ * decla) list
    env est de type (string * typ) list *)
-let rec eval_declaration tl env = 
+let rec eval_declaration l env = 
   match l with
-  | (s, t, d)::tl -> let env = Env.add s Typ(t) env in
+  | (s, t, d)::tl -> let env = Env.add s (Typ t) env in
                       eval_expr d env;
                       eval_declaration tl env
   | [] -> env
 ;;
-
-
-let rec extract_typ p l =
-  match p with
-  | (str,tp)::tl -> extract_typ tl tp::l 
-  | _ -> l
-;;
-
 
 (**Renvoie un env avec les fonctions rajouter à l'env,
   vérifie que les var locals et les seqs sont bien typé,
@@ -54,25 +146,15 @@ let rec extract_typ p l =
 
 let rec eval_functions l env =
   match l with
-  | f::tl -> let lst_typ = extratc_typ f.params [] in
-             let env = Env.add f.name TypFun(lst_typ, f.return) env in
-             let env_loc = eval_params f.params env in
+  | f::tl -> let lst_typ = extract_typ (f.params) [] in
+             let env = Env.add (f.name) (TypFun (lst_typ, f.return)) env in
+             let env_loc = eval_params (f.params) env in
              let env_loc = eval_declaration f.locals env in
              eval_seq f f.code env_loc;
              eval_functions tl env
   | [] -> env (*ou () ?*)
 ;;
 
-
-(*Renvoie un env, pas besoin d'evaluation ici
-  l est de type (string * typ) list
-  env est de type (string * typ) list *)
-let rec eval_params l env =
-  match l with
-  | (s,t)::tl -> let env = Env.add s Typ(t) env in
-                 eval_params tl env
-  | [] -> env
-;;
 
 
 (*f = fonction qu'on evalue
@@ -116,51 +198,11 @@ let rec eval_instr f instr env =
 
 
 
-(**Compare 2 listes et renvoie true si egale false sinon**)
-let rec compare_type l0 l1 env =
-  match l0, l1 with
-  | (ta::tl0, tb::tl1) -> let tb = eval_expr b env in
-                           (ta = tb) && compare_type tl0 tl1 env 
-  | ([], []) -> true
-  | _ -> false
+
+(*vérifie que le prog est bien typé
+  p est de type prog
+  env est de type (string * typ) list *)
+let rec eval_prog p env =
+  let env = eval_declaration p.globals env in
+  eval_functions p.functions env
 ;;
-
-
-(**Evalue les expressions**)
-let rec type_expr (e: expr) (env: typ Env.t): typ = match e with
-
-  | Cst _ -> Typ(Int)
-
-  | Add(e1, e2) ->
-    let t1 = type_expr e1 env in
-    let t2 = type_expr e2 env in
-    if t1 = Typ(Int) && t2 = Typ(Int)
-    then Typ(Int)
-    else failwith "type error"
-
-  | Mul(e1, e2) ->
-    let t1 = type_expr e1 env in
-    let t2 = type_expr e2 env in
-    if t1 = Typ(Int) && t2 = Typ(Int)
-    then Typ(Int)
-    else failwith "type error"
-
-  | Lt(e1, e2) ->
-    let t1 = type_expr e1 env in
-    let t2 = type_expr e2 env in
-    if t1 = Typ(Int) && t2 = Typ(Int)
-    then Typ(Bool)
-    else failwith "type error"
-
-  | Get(s) -> Env.find s env
-
-  | Call(f, arg) ->
-    let tf = Env.find f env in
-    begin match tf with
-    | TypFun(l, tr) -> if compare_type l arg
-                       then tr
-                       else failwith "type error"
-    | _ -> failwith "type error"
-    end
-;;
-
