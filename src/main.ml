539b7e30a9a31e21f16a8f26812719de42d02ba2
adblackx <adblackx@gmail.com>
2020-12-19 18:01:52 +0100

    update main

--- src/main.ml
+++ src/main.ml
@@ -3,7 +3,7 @@ let () =
   let c = open_in fichier in
   let lexbuf = Lexing.from_channel c in
   let prog = Mini_c_parser.prog Mini_c_lexer.token lexbuf in
-  let test = Mini_c_types.type_expr prog Mini_c_types.Env.empty in
+  let test = Mini_c_types.eval_prog prog Mini_c_types.Env.empty in
   ignore(prog);
   close_in c;
   exit 0
