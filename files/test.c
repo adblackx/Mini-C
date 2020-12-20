864c6ec11246582772eb88e54b9076894e04c32b
Romain <romainmussard974@gmail.com>
2020-12-20 12:12:38 +0100

    Modif test.c

--- files/test.c
+++ files/test.c
@@ -1,14 +1,14 @@
  int PARAM = 5;
  bool bobo = true;
-  int fact(int n, bool simone) {
+  int fact(int n) {
     bool toto = false;
     if (n < 2) {
       return 1;
     } else {
-      return n * fact(n + 1, bobo);
+      return n * fact(n + 1);
     }
   }
 
   void main() {
-    putchar(fact(PARAM, bobo));
+    putchar(fact(PARAM));
   }
