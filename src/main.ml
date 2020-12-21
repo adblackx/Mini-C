936d916acc63e2af58e3e31b1bae1ae08eb51f24
Romain <romainmussard974@gmail.com>
2020-12-22 00:29:39 +0100

    Rajout commentaire main

--- src/main.ml
+++ src/main.ml
@@ -8,7 +8,7 @@ let () =
   let lexbuf = Lexing.from_channel c in
   let prog = Mini_c_parser.prog Mini_c_lexer.token lexbuf in
   Test_prog.print_prog prog ; (*commenter tout cette ligne pour enlever le print*) 
-  let _ = Mini_c_types.start_eval prog in
+  let _ = Mini_c_types.start_eval prog in (**Commenter cette ligne pour ne pas faire de verification de type**)
   ignore(prog);
   close_in c;
  
